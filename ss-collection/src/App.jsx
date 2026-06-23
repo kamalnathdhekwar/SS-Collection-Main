8882317afddd40a93194f8b9d2327e5587e28113
aryan-karande <aryan.karande2006@gmail.com>
2026-06-23 21:43:37 +0530

    react and tailwind (from demo website)

---
+++ ss-collection/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+export default App
